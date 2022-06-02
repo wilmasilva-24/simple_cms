3a6d46dfba60df4704125296bb96d2db3aba6017
wilma <wilminhasilva@gmail.com>
2022-06-02 20:34:49 -0300

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root 'demo#index'
+  
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
+
+  # default route
+  #get ':controller(/:action(/:id))'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
