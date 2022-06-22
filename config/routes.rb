0e3a5e809fe87b1f9a94812bcfa0402cd9b29077
wilma <wilminhasilva@gmail.com>
2022-06-21 21:42:54 -0300

    CRUD, REST and Resourceful Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'demo#index'
   
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
   get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
