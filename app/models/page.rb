0e1917535e54f0c777fd63608881649bf7bc9412
wilma <wilminhasilva@gmail.com>
2022-06-28 21:19:05 -0300

    Controllers and CRUD

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,9 @@
 class Page < ApplicationRecord
-    belongs_to :subject
-    has_and_belongs_to_many :admin_users
+  belongs_to :subject
+  has_and_belongs_to_many :admin_users
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("position ASC") }
+  scope :newest_first, lambda { order("created_at DESC") }
 end
-# { :optional => false }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'sections/index'
+  get 'sections/show'
+  get 'sections/new'
+  get 'sections/edit'
+  get 'sections/delete'
+  get 'pages/index'
+  get 'pages/show'
+  get 'pages/new'
+  get 'pages/edit'
+  get 'pages/delete'
   root 'demo#index'
   
   resources :subjects do
@@ -6,6 +16,18 @@ Rails.application.routes.draw do
       get :delete
     end
   end
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+
+  resources :sections do
+    member do
+      get :delete
+    end
+  end
+  
   get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
